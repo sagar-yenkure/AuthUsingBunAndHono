2e0d359a48ff378b71e9f04c2db9e4ff4af091c9
sagar yenkure <yenkuresagar2104@gmail.com>
2024-06-30 19:22:11 +0530

    vercel url added

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import auth from "./routes/auth.routes";
 import { logger } from "hono/logger";
+import { handle } from "hono/vercel";
+export const runtime = "edge";
+const APP = new Hono().basePath("/api");
 
-const APP = new Hono();
 APP.use(logger());
 
-const server = Bun.serve({
+export const server = Bun.serve({
   port: process.env.HOST || 3001,
   fetch: APP.fetch,
 });
@@ -15,3 +17,5 @@ const server = Bun.serve({
 APP.route("/api/auth", auth);
 
 console.log(`The server is running on port ${server.port}`);
+export const GET = handle(APP);
+export const POST = handle(APP);
